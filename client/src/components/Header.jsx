fc532bd08238a43db8151f33769988fbec61aef6
henriquet <henriquetkapema@gmail.com>
2024-04-09 14:49:19 +0100

    create the header section 3

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -37,17 +37,18 @@ export default function Header() {
             <Navbar.Toggle /> 
         </div>
             <Navbar.Collapse>
+                
                 <Navbar.Link active={path === "/"} as={'div'}>
                     <Link to='/'>
                         Home
                     </Link>
                 </Navbar.Link >
-                <Navbar.Link active={path === "/about"} as={div}>
+                <Navbar.Link active={path === "/about"} as={'div'}>
                     <Link to='/about'>
                         About
                     </Link>
                 </Navbar.Link>
-                <Navbar.Link active={path === "/projects"}as={div}>
+                <Navbar.Link active={path === "/projects"}as={'div'}>
                     <Link to='/projects'>
                        Projects
                     </Link>
