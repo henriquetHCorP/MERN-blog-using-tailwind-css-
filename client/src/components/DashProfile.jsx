68531f0b4bfcc8c3864b86e638a659368a4a7813
henriquet <henriquetkapema@gmail.com>
2024-04-19 22:02:08 +0100

    add update user api route

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -19,7 +19,7 @@ export default function DashProfile() {
         const file = e.target.files[0];  
         if (file) {
             setImageFile(file); 
-            setImageFileUrl(URL.createObjectURL(file)); 
+            setImageFileUrl(URL.createObjectURL(file));   
         }
         // console.log(imageFile,"image Url:", imageFileUrl); 
      }; 
