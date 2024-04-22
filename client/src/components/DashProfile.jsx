5dd75a8c4b477ebd379f9b00a9963520cc5bee5e
henriquet <henriquetkapema@gmail.com>
2024-04-22 23:31:31 +0100

    complete create a post page UI

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage';
 import { app } from '../firebase';  
 import { CircularProgressbar } from 'react-circular-progressbar';
+import { Link } from 'react-router-dom';
 import 'react-circular-progressbar/dist/styles.css';
 import {
   updateStart, 
@@ -20,7 +21,7 @@ import {HiOutlineExclamationCircle} from 'react-icons/hi'
 export default function DashProfile() {
 
   
-  const {currentUser, error} = useSelector(state => state.user)
+  const {currentUser, error, loading} = useSelector(state => state.user)
   const [imageFile, setImageFile ] = useState(null); 
   const [imageFileUrl, setImageFileUrl] = useState(null); 
   const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null); 
@@ -227,9 +228,25 @@ export default function DashProfile() {
           type="password" 
           id="password"
           placeholder="password" onChange={handleChange} />
-          <Button type="submit" gradientDuoTone='purpleToBlue' outline>
-                Update
+          <Button 
+             type="submit" 
+             gradientDuoTone='purpleToBlue' 
+             outline
+             disabled={loading || imageFileUploading}
+             >
+               {loading ? 'Loading...' : 'Update'}
           </Button>
+          {currentUser.isAdmin && (
+            <Link to={'/create-post'}> 
+            <Button
+              type='button'
+              gradientDuoTone='purpleToPink'
+              className='w-full'
+            >
+                Create a post 
+            </Button>
+            </Link>
+          )}
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span onClick ={() => setShowModal(true)} className="cursor-pointer"> Delete Account</span>
