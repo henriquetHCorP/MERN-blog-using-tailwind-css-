5d7a745100fddbe87970c525b0ce19a527b5d042
henriquet <henriquetkapema@gmail.com>
2024-04-29 01:13:44 +0100

    add call to action to the post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,25 @@
+import { Button } from 'flowbite-react'
+import React from 'react'
+
+export default function CallToAction() {
+  return (
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+      <div className="flex-1 justify-center flex flex-col">
+        <h2 className="text-2xl">
+            Want to learn more about the war in the DRC? 
+        </h2>
+        <p classNme="text-gray-500 my-2">
+            Checkout these resources with 100 local correspondants 
+        </p>
+        <Button gradientDuoTone="purpleToPink" className="rounded-tl-xl rounded-bl-none">
+            {/* here we are using achored tag instead of link because this will open an external link */}
+           <a href="https://www.tele50.cd" target='_blank' rel='noopener noreferrer'> Learn More</a>
+            
+        </Button>
+        </div>
+        <div className="p-7 flex-1">
+             <img src="https://cdn.cfr.org/sites/default/files/styles/open_graph_article/public/image/2023/03/Burundi%20Soldiers%2003142023%20.jpeg"/> 
+        </div> 
+    </div>
+  )
+}
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
 
 
 export default function PostPage() {
@@ -62,6 +63,9 @@ export default function PostPage() {
     </div>
     {/* for the post-content className below, go to index.css for styling  */}
     <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html:post && post.content}}>
+    </div>
+    <div className="">
+        <CallToAction /> 
 
     </div>
     </main>
