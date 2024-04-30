8aa1a069da3c096e265e9654928eaeeac47af49f
henriquet <henriquetkapema@gmail.com>
2024-04-30 01:11:34 +0100

    add comment section to the post page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
 import PostPage from './pages/PostPage';
+import ScrollToTop from './components/ScrollToTop';
 
 
 
@@ -22,6 +23,7 @@ export default function App() {
     //   App
     // </div>
     <BrowserRouter>
+    <ScrollToTop />
     <Header />
       <Routes>
         <Route path="/" element={<Home /> } />
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from "../components/CallToAction";
+import CommentSection from "../components/CommentSection";
 
 
 export default function PostPage() {
@@ -64,10 +65,10 @@ export default function PostPage() {
     {/* for the post-content className below, go to index.css for styling  */}
     <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html:post && post.content}}>
     </div>
-    <div className="">
+    <div className="max-w-4xl mx-auto w-full"> 
         <CallToAction /> 
-
     </div>
+    <CommentSection postId={post._id} /> 
     </main>
   )
 }
