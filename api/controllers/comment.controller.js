7121069608b528409ef04921258fe4aa867f45e7
henriquet <henriquetkapema@gmail.com>
2024-05-03 00:47:35 +0100

    add edit functionality to the comment component

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,4 +1,5 @@
 import Comment from "../models/comment.model.js";
+import { errorHandler } from "../utils/error.js";
 
 //  import  Comment  from "/models/comment.model.js"
 export const createComment = async (req, res, next) => {
@@ -58,3 +59,24 @@ export const likeComment = async (req, res, next) => {
   }
 }
 
+export const editComment = async (req, res, next) => {
+   try {
+        const comment = await Comment.findById(req.params.commentId);
+        if(!comment){
+          return next(errorHandler(404, 'Comment not found')); 
+        }
+        if(comment.userId !== req.user.id && !user.role.isAdmin){
+          return next(errorHandler(403, 'You are not allowed to edit this comment')); 
+        }
+        const editedComment = await Comment.findByIdAndUpdate(
+          req.params.commentId, 
+          {
+            content: req.body.content
+          }, 
+          { new: true }
+        ); 
+        res.status(200).json(editedComment)
+   }catch(error) {
+    next(error)
+   }
+}
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react'; 
 import moment from 'moment'; 
-import { Button } from 'flowbite-react';
+import { Button, Textarea } from 'flowbite-react';
 import { FaThumbsUp} from 'react-icons/fa'; 
 import { useSelector } from 'react-redux';
 
-export default function Comment({comment, onLike}) {
+export default function Comment({comment, onLike, onEdit}) {
     const [user, setUser ] = useState({}); 
-
+    const [isEditing, setIsEditing] = useState(false); 
+    const [editedContent, setEditedContent] = useState(comment.content); 
     const {currentUser} = useSelector((state) => (state.user)); 
-    console.log(user); 
+
+    // console.log(user); 
     useEffect(() => {
          const getUser = async () => {
             try {
@@ -24,8 +26,35 @@ export default function Comment({comment, onLike}) {
          getUser(); 
     }, [comment])
    
+  const handleEdit = () => {
+    setIsEditing(true); 
+    setEditedContent(comment.content); 
+
+  }
+
+  const handleSave = async () => {
+    try {
+      const res = await fetch(`/api/comment/editComment/${comment._id}`, {
+        method:'PUT', 
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          content:editedContent
+        })
+      });
+      if(res.ok) {
+        setIsEditing(false); 
+        onEdit(comment, editedContent)
+      } 
+    } catch(error) {
+      console.log(error.message); 
+    }
+
+
+  }
+
   return (
-    
     <div className="flex p-4 border-b dark:border-gray-600 text-sm">
         <div className="flex-shrink-0 mr-3">
             <img  className="w-10 h-10 rounded-full bg-gray-200" 
@@ -40,7 +69,37 @@ export default function Comment({comment, onLike}) {
                 <span className="font-bold mr-1 text-xs truncate">{user ? `@${user.username}` : 'anonymous user'}</span>
                 <span className="text-gray-500 text-xs">{moment(comment.createdAt).fromNow()}</span>
             </div>
-            <p className="text-gray-500 pb-2">{comment.content}</p>
+            {isEditing? (
+              <>
+              <Textarea
+               className="mb-2"
+               value={editedContent}
+               onChange={(e) => setEditedContent(e.target.value)}
+              />
+              <div className="flex justify-end gap-2 text-xs">
+                <Button
+                  type='button'
+                  size='sm'
+                  gradientDuoTone='purpleToBlue'
+                  onClick={handleSave}
+                >
+                    Save 
+                </Button>
+                <Button
+                  type='button'
+                  size='sm'
+                  gradientDuoTone='purpleToBlue'
+                  outline
+                  onClick= {() => setIsEditing(false)}
+                >
+                    Cancel 
+                </Button>
+              </div>
+              </>
+            ) : ( 
+              
+          <>
+          <p className="text-gray-500 pb-2">{comment.content}</p>
             <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
               <button 
                  type='button' 
@@ -53,7 +112,23 @@ export default function Comment({comment, onLike}) {
                     comment.numberOfLikes > 0 && comment.numberOfLikes + " " +(comment.numberOfLikes === 1 ? "like" : "likes")
                   }
                 </p>
+                {
+                  currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
+                    <button
+                      type='button'
+                      clasName="text-gray-400 hover: text-blue-500"
+                      onClick={handleEdit}
+                      // we dont't need to create and handleEdit function in the CommentSection because doing so will affect all the others comments. we just need to update this comment 
+                    >
+                          Edit 
+                    </button>
+                  )
+                }
             </div>
+          </>
+            )  
+            }
+            
         </div>
      
     </div>
