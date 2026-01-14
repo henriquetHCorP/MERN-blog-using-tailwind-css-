8697a9c60fcfb596221f475c60d05b6d3dddfbe3
henriquet <henriquetkapema@gmail.com>
2026-01-14 11:10:42 +0100

    "Veuillez saisir vos identifiants animate-slow-blink"

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -13,8 +13,16 @@ export default {
     extend: {
       animation: {
         'blink': 'blink 1s steps(1, end) infinite', // Use 'steps(1, end)' for an instant on/off blink
+        'slow-blink': 'slow-blink 3s linear infinite', // 3s duration, linear timing, infinite loop
+
       },
+      
+
       keyframes: {
+        'slow-blink': {
+          '0%, 100%': { opacity: '0' },
+          '50%': { opacity: '1' },
+        },
         blink: {
           '0%, 100%': { opacity: '1' }, // Visible at 0% and 100%
           '50%': { opacity: '0' },      // Invisible at 50%
