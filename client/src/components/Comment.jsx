be1418eda892b34247c7d67b2d71ec0d6fead9d3
henriquet <henriquetkapema@gmail.com>
2026-01-31 08:35:16 +0100

    reinstate comment amended

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -1,143 +1,245 @@
-import moment from 'moment';
-import { useEffect, useState } from 'react';
-import { FaThumbsUp } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
-import { Button, Textarea } from 'flowbite-react';
-import { set } from 'mongoose';
+import React, { useEffect, useState } from 'react'; 
+import moment from 'moment'; 
 
-export default function Comment({ comment, onLike, onEdit, onDelete }) {
-  const [user, setUser] = useState({});
-  const [isEditing, setIsEditing] = useState(false);
-  const [editedContent, setEditedContent] = useState(comment.content);
-  const { currentUser } = useSelector((state) => state.user);
-  useEffect(() => {
-    const getUser = async () => {
-      try {
-        const res = await fetch(`/api/user/${comment.userId}`);
-        const data = await res.json();
-        if (res.ok) {
-          setUser(data);
-        }
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-    getUser();
-  }, [comment]);
+
+
+import { Button, Textarea } from 'flowbite-react';
+import { FaThumbsUp} from 'react-icons/fa'; 
+import { useSelector } from 'react-redux';
+
+import { redirect, useNavigate, useParams } from 'react-router-dom'
+
+
+moment.locale('fr', {
+    months : 'janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre'.split('_'),
+    monthsShort : 'janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.'.split('_'),
+    monthsParseExact : true,
+    weekdays : 'dimanche_lundi_mardi_mercredi_jeudi_vendredi_samedi'.split('_'),
+    weekdaysShort : 'dim._lun._mar._mer._jeu._ven._sam.'.split('_'),
+    weekdaysMin : 'Di_Lu_Ma_Me_Je_Ve_Sa'.split('_'),
+    weekdaysParseExact : true,
+    longDateFormat : {
+        LT : 'HH:mm',
+        LTS : 'HH:mm:ss',
+        L : 'DD/MM/YYYY',
+        LL : 'D MMMM YYYY',
+        LLL : 'D MMMM YYYY HH:mm',
+        LLLL : 'dddd D MMMM YYYY HH:mm'
+    },
+    calendar : {
+        sameDay : '[Aujourd’hui à] LT',
+        nextDay : '[Demain à] LT',
+        nextWeek : 'dddd [à] LT',
+        lastDay : '[Hier à] LT',
+        lastWeek : 'dddd [dernier à] LT',
+        sameElse : 'L'
+    },
+    relativeTime : {
+        future : 'dans %s',
+        past : 'il y a %s',
+        s : 'quelques secondes',
+        m : 'une minute',
+        mm : '%d minutes',
+        h : 'une heure',
+        hh : '%d heures',
+        d : 'un jour',
+        dd : '%d jours',
+        M : 'un mois',
+        MM : '%d mois',
+        y : 'un an',
+        yy : '%d ans'
+    },
+    dayOfMonthOrdinalParse : /\d{1,2}(er|e)/,
+    ordinal : function (number) {
+        return number + (number === 1 ? 'er' : 'e');
+    },
+    meridiemParse : /PD|MD/,
+    isPM : function (input) {
+        return input.charAt(0) === 'M';
+    },
+    // In case the meridiem units are not separated around 12, then implement
+    // this function (look at locale/id.js for an example).
+    // meridiemHour : function (hour, meridiem) {
+    //     return /* 0-23 hour, given meridiem token and hour 1-12 */ ;
+    // },
+    meridiem : function (hours, minutes, isLower) {
+        return hours < 12 ? 'PD' : 'MD';
+    },
+    week : {
+        dow : 1, // Monday is the first day of the week.
+        doy : 4  // Used to determine first week of the year.
+    }
+});
+
+
+
+
+export default function Comment({comment, onLike, onEdit, onDelete}) {
+    const [user, setUser ] = useState({}); 
+    const [isEditing, setIsEditing] = useState(false); 
+    const [editedContent, setEditedContent] = useState(comment.content); 
+    const {currentUser} = useSelector((state) => (state.user)); 
+    
+    // console.log(user); 
+    useEffect(() => {
+         const getUser = async () => {
+            try {
+               const res = await fetch(`/api/user/${comment.userId}`); 
+               const data = await res.json(); 
+               if(res.ok) {
+                setUser(data); 
+               }
+            }catch(error) {
+                console.log(error.message)
+            }
+         } 
+         getUser(); 
+    }, [comment]); 
+
+  
+  const filterUrls = (text) => {
+    // Regex finds common URL patterns (http/https/ftp and www. or just text.domain)
+    const urlRegex = /(https?:\/\/[^\s]+|www\.[^\s]+|[a-zA-Z0-9]+\.[^\s]+)/gi;
+    return text.replace(urlRegex, '[Lien retiré]');
+  };
 
   const handleEdit = () => {
-    setIsEditing(true);
-    setEditedContent(comment.content);
-  };
+    setIsEditing(true); 
+    setEditedContent(comment.content); 
+
+  }
 
   const handleSave = async () => {
     try {
       const res = await fetch(`/api/comment/editComment/${comment._id}`, {
-        method: 'PUT',
+        method:'PUT', 
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          content: editedContent,
-        }),
+          content:editedContent
+        })
       });
-      if (res.ok) {
-        setIsEditing(false);
-        onEdit(comment, editedContent);
-      }
-    } catch (error) {
-      console.log(error.message);
+      if(res.ok) {
+        setIsEditing(false); 
+        onEdit(comment, editedContent)
+      } 
+    } catch(error) {
+      console.log(error.message); 
     }
-  };
+
+
+  }
+
+  const navigate = useNavigate(); 
+  
+  //  console.log("user:", user); 
   return (
-    <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
-      <div className='flex-shrink-0 mr-3'>
-        <img
-          className='w-10 h-10 rounded-full bg-gray-200'
-          src={user.profilePicture}
-          alt={user.username}
-        />
-      </div>
-      <div className='flex-1'>
-        <div className='flex items-center mb-1'>
-          <span className='font-bold mr-1 text-xs truncate'>
-            {user ? `@${user.username}` : 'anonymous user'}
-          </span>
-          <span className='text-gray-500 text-xs'>
-            {moment(comment.createdAt).fromNow()}
-          </span>
+    <>
+    {/* <div className="flex gap-1 justify-end">
+            <button type="button" onClick={() => onPostLike(post._Id)} className="text-gray-400 hover:text-blue-500">
+              <FaThumbsUp className="text-xl"/> 
+            </button>
+            <p>Like this Post</p> 
+          </div> */}
+          
+    <div className="flex p-4 border-b dark:border-gray-600 text-sm">
+      
+        <div className="flex-shrink-0 mr-3">
+          
+            <img  className="w-10 h-10 rounded-full bg-gray-200 cursor-pointer" 
+              src={user.profilePicture} 
+              //alt={user.username}
+              alt="Photo" 
+              onClick={() => currentUser ? navigate(`/user/${user._id}`): navigate('/sign-in')}
+              /> 
         </div>
-        {isEditing ? (
-          <>
-            <Textarea
-              className='mb-2'
-              value={editedContent}
-              onChange={(e) => setEditedContent(e.target.value)}
-            />
-            <div className='flex justify-end gap-2 text-xs'>
-              <Button
-                type='button'
-                size='sm'
-                gradientDuoTone='purpleToBlue'
-                onClick={handleSave}
-              >
-                Save
-              </Button>
-              <Button
-                type='button'
-                size='sm'
-                gradientDuoTone='purpleToBlue'
-                outline
-                onClick={() => setIsEditing(false)}
-              >
-                Cancel
-              </Button>
+        <div className="flex-1">
+            {/* <div > */}
+            <div className ="flex items-center mb-1">
+                {/* below, anonymous user in case the user has been deleted  */}
+                <span className="font-bold mr-1 text-xs truncate">{user ? `@${user.username}` : 'Utilisateur supprimé'}</span>
+                <span className="text-gray-500 text-xs">
+                  
+                  {moment(comment.createdAt).fromNow()}</span>
             </div>
-          </>
-        ) : (
+            {isEditing? (
+              <>
+              <Textarea
+               className="mb-2"
+               value={editedContent}
+               onChange={(e) => setEditedContent(e.target.value)}
+              />
+              <div className="flex justify-end gap-2 text-xs">
+                <Button
+                  type='button'
+                  size='sm'
+                  gradientDuoTone='purpleToBlue'
+                  onClick={handleSave}
+                  disabled={editedContent === ""  || editedContent.trim() === "" || editedContent.includes('AFC') || editedContent.includes('..') || editedContent.includes('M23') || editedContent.includes('Afc') || editedContent.includes('afc') || editedContent.includes('M23') || editedContent.includes('m23') || editedContent.includes('M 2 3') || editedContent.includes('m 2 3') || editedContent.includes('KAGAME') || editedContent.includes('Kagame') || editedContent.includes('kagame') || editedContent.includes('NANGA') || editedContent.includes('Nanga') || editedContent.includes('NANGAA') || editedContent.includes('Nangaa') || editedContent.includes('nanga') || editedContent.includes('Willy Ngoma') || editedContent.includes('VISIT RWANDA') || editedContent.includes('Visit Rwanda') || editedContent.includes('Alliance Fleuve Congo') || editedContent.includes('alliance fleuve congo') || editedContent.includes('Alliance Fleuve Congo') || editedContent.includes('ALLIANCE FLEUVE CONGO') || editedContent.includes('M-23') || editedContent.includes('M/23') || editedContent.includes('m-23') || editedContent.includes('m/ 2 3') || editedContent.includes('M/ 2 3') || editedContent.includes('m/23') || editedContent.includes('Alliance fleuve congo') || editedContent.includes('Alliance Fleuve congo') || editedContent === ""  || editedContent.trim() === "" || editedContent.replace(/\s/g, '').toLowerCase().includes('afc') || editedContent.replace(/\s/g, '').toLowerCase().includes('m23') || editedContent.replace(/\s/g, '').toLowerCase().includes('kagame') || editedContent.replace(/\s/g, '').toLowerCase().includes('nanga') || editedContent.replace(/\s/g, '').toLowerCase().includes('nangaa') || editedContent. replace(/\s/g, '').toLowerCase().includes('willyngoma') || editedContent.replace(/\s/g, '').toLowerCase().includes('visitrwanda') || editedContent.replace(/\s/g, '').toLowerCase().includes('alliancefleuvecongo') || editedContent.replace(/\s/g, '').toLowerCase().includes('m-23') || editedContent.replace(/\s/g, '').toLowerCase().includes('m/23') || editedContent.replace(/[\s-_%@#$%^&*/\\.]/g, '').toLowerCase().includes('nanga') || editedContent.replace(/[\s-_%@#$%^&*/\\]/g, '').toLowerCase().includes('corneillen.') || editedContent.replace(/[\s-_%@#$%^&*/\\.]/g, '').toLowerCase().includes('afc') || editedContent.replace(/[\s-_%@#$%^&*/\\.]/g, '').toLowerCase().includes('m23') || editedContent.replace(/[\s-_%@#$%^&*/\\.]/g, '').toLowerCase().includes('alliancefleuvecongo') || editedContent.replace(/[\s-_%@#$%^&*/\\.]/g, '').toLowerCase().includes('kagame') || editedContent.replace(/[\s-_%@#$%^&*/\\.]/g, '').toLowerCase().includes('willyngoma') || editedContent.replace(/[\s-_%@#$%^&*/\\.]/g, '').toLowerCase().includes('visitrwanda')}
+                >
+                    Enregister 
+                </Button>
+                <Button
+                  type='button'
+                  size='sm'
+                  gradientDuoTone='purpleToBlue'
+                  outline
+                  onClick= {() => setIsEditing(false)}
+                >
+                    Annuler
+                </Button>
+              </div>
+              </>
+            ) : ( 
+              
           <>
-            <p className='text-gray-500 pb-2'>{comment.content}</p>
-            <div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
-              <button
-                type='button'
-                onClick={() => onLike(comment._id)}
-                className={`text-gray-400 hover:text-blue-500 ${
-                  currentUser &&
-                  comment.likes.includes(currentUser._id) &&
-                  '!text-blue-500'
-                }`}
-              >
-                <FaThumbsUp className='text-sm' />
-              </button>
-              <p className='text-gray-400'>
-                {comment.numberOfLikes > 0 &&
-                  comment.numberOfLikes +
-                    ' ' +
-                    (comment.numberOfLikes === 1 ? 'like' : 'likes')}
-              </p>
-              {currentUser &&
-                (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                  <>
+          <p className="text-gray-500 pb-2">{filterUrls(comment.content)}</p>
+            <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+              <button 
+                 type='button' 
+                 onClick = {() => onLike(comment._id)}
+                 className={`text-gray-400 hover:text-blue-500 ${currentUser && comment.likes.includes(currentUser._id) && '!text-blue-500'}`}>
+                <FaThumbsUp  className="text-sm"/> 
+                </button>
+                {/* <button className="cursor-pointer" onClick={() => currentUser ? navigate(`/user/${user._id}`): navigate('/sign-in')}>Répondre</button> */}
+                <p className="text-gray-400">
+                  
+                  {
+                    comment.numberOfLikes > 0 && comment.numberOfLikes + " " +(comment.numberOfLikes === 1 ? "like" : "likes")
+                  }
+                </p>
+                {/* below here i added the delete and edit button to the user info page owner  */}
+                {
+                  currentUser && (currentUser._id === comment.userId || currentUser.isAdmin || currentUser._id === userId) && (
+                    <>
                     <button
                       type='button'
+                      className="text-gray-400 hover:text-blue-500"
                       onClick={handleEdit}
-                      className='text-gray-400 hover:text-blue-500'
+                      // we dont't need to create and handleEdit function in the CommentSection because doing so will affect all the others comments. we just need to update this comment 
                     >
-                      Edit
+                          Editer
                     </button>
                     <button
-                      type='button'
-                      onClick={() => onDelete(comment._id)}
-                      className='text-gray-400 hover:text-red-500'
-                    >
-                      Delete
-                    </button>
+                    type='button'
+                    onClick={() => onDelete(comment._id)}
+                    className="text-gray-400 hover:text-red-500"
+                    // we dont't need to create and handleEdit function in the CommentSection because doing so will affect all the others comments. we just need to update this comment 
+                  >
+                        Supprimer
+                  </button>
                   </>
-                )}
+                  )
+                }
             </div>
           </>
-        )}
-      </div>
+            )  
+            }
+            
+        </div>
+     
     </div>
-  );
+    </>
+  )
 }
