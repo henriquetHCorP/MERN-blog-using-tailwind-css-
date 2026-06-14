f3d447303ef354757191c2ae3663b6ec2a76f90d
henriquet <henriquetkapema@gmail.com>
2026-06-14 13:03:58 +0100

    changing the address of socketjs from localhost to deployed url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,3 +1,6 @@
 // src/socket.js
 import { io } from 'socket.io-client';
-export const socket = io('http://localhost:5000', { autoConnect: false });
+
+    // export const socket = io('http://localhost:5000', { autoConnect: false });
+
+export const socket = io('https://drc-gov-social-media.onrender.com', { autoConnect: false });
